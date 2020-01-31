5a76d217a773b75dea273fa1ee6b5bd94df49ddf
Jorge Vazquez <jorge.vazquez.acevedo@alumnos.upm.es>
2020-01-31 15:42:38 +0100

    Incluir Logger + Test ejemplo

--- src/main/java/ingsoft1920/em/EmApplication.java
+++ src/main/java/ingsoft1920/em/EmApplication.java
@@ -1,12 +1,16 @@
 package ingsoft1920.em;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EmApplication {
+	final static Logger logger = LogManager.getLogger(EmApplication.class.getName());
 
 	public static void main(String[] args) {
+		logger.warn("Aplicacion iniciada");
 		SpringApplication.run(EmApplication.class, args);
 	}
 
