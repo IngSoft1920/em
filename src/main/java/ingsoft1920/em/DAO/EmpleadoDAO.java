ad0b68c29812dd013b64fba06ba74deb3b1e1272
Rebeca <rebeca.carrasco.moreno@alumnos.upm.es>
2020-02-17 10:02:39 +0100

    RellenoDAOempleado
    probando rellenar dao empleado

--- src/main/java/ingsoft1920/em/DAO/EmpleadoDAO.java
+++ src/main/java/ingsoft1920/em/DAO/EmpleadoDAO.java
@@ -1,4 +1,14 @@
 package ingsoft1920.em.DAO;
 public class EmpleadoDAO {
-
+	
+	public static List<DatoEmpleadoBean> sacaEmpleados(){
+		BeanListHandler<DatoEmpleadoBean> handler=new BeanListHandler<>(DatoEmpleadoBean.class);
+		QueryRunner runner=new QueryRunner();
+		String query="SELECT * FROM empleado";
+		try {
+			res=runner.query(ConectorBBDD.conectar(), query,handler);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
