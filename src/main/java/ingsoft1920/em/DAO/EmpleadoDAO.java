815f3709d5ea8ecf9ad46a40289a2620fec288a1
Rebeca <rebeca.carrasco.moreno@alumnos.upm.es>
2020-02-17 19:29:34 +0100

    Cambios models y DAO

--- src/main/java/ingsoft1920/em/DAO/EmpleadoDAO.java
+++ src/main/java/ingsoft1920/em/DAO/EmpleadoDAO.java
@@ -1,14 +1,96 @@
 package ingsoft1920.em.DAO;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class EmpleadoDAO {
-	
-	public static List<DatoEmpleadoBean> sacaEmpleados(){
-		BeanListHandler<DatoEmpleadoBean> handler=new BeanListHandler<>(DatoEmpleadoBean.class);
-		QueryRunner runner=new QueryRunner();
-		String query="SELECT * FROM empleado";
-		try {
-			res=runner.query(ConectorBBDD.conectar(), query,handler);
-		} catch (SQLException e) {
-			e.printStackTrace();
+	//TODAS las consultas a las bases de datos
+	public static EmpleadoModelC1 sacaEmpleados(){
+		//CONSULTA1-> Hay que enviar a dho todos los id_empleado con su id_turno
+		EmpleadoModelC1 res = null;
+		Statement stmt = null; 
+		ResultSet rs = null; 
+		  try { 
+		   stmt = conn.createStatement() ;
+		   rs = stmt.executeQuery("SELECT * FROM empleado);
+		   if (rs.next()){
+		      res = new EmpleadoModelC1 ( 
+		      rs.getInt("id_Empleado"), 
+		      rs.getInt("id_Turno"));
+		      }
+          } 
+		  catch (SQLException ex){ 
+		   System.out.println("SQLException: " + ex.getMessage());
+		  }
+		  return res;
 		}
+	public static EmpleadoModelC2 sacaEmpleados2() {
+		//CONSULTA2-> Hay que enviar a fnb id_Empleado con su id_Rol y estado
+		EmpleadoModelC2 res = null;
+		Statement stmt = null; 
+		ResultSet rs = null; 
+		  try { 
+		   stmt = conn.createStatement() ;
+		   rs = stmt.executeQuery("SELECT * FROM empleado);
+		   if (rs.next()){
+		      res = new EmpleadoModelC2 ( 
+		      rs.getInt("id_Empleado"), 
+		      rs.getString("id_Rol"),
+		      rs.getInt("estado"));
+		      }
+          } 
+		  catch (SQLException ex){ 
+		   System.out.println("SQLException: " + ex.getMessage());
+		  }
+		  return res;
+	}
+	public static BajaModel sacaBajas() {
+		//CONSULTA3-> Hay que enviar a cm las bajas con el id_empleado que la pide y su duración
+		BajaModel res = null;
+		Statement stmt = null; 
+		ResultSet rs = null; 
+		  try { 
+		   stmt = conn.createStatement() ;
+		   rs = stmt.executeQuery("SELECT * FROM bajas);
+		   if (rs.next()){
+		      res = new BajaModel ( 
+		      rs.getInt("id_Empleado"), 
+		      rs.getInt("id_Baja"),
+		      rs.getInt("duracion"));
+		      }
+          } 
+		  catch (SQLException ex){ 
+		   System.out.println("SQLException: " + ex.getMessage());
+		  }
+		  return res;
+	}
+	
+	public static VacacionesModel sacaBajas() {
+		//CONSULTA4-> Hay que enviar a cm las vacacioens con el id_empleado que la pide y su duración
+		VacacionesModel res = null;
+		Statement stmt = null; 
+		ResultSet rs = null; 
+		  try { 
+		   stmt = conn.createStatement() ;
+		   rs = stmt.executeQuery("SELECT * FROM vacaciones);
+		   if (rs.next()){
+		      res = new BajaModel ( 
+		      rs.getInt("id_Empleado"), 
+		      rs.getInt("id_Vacaciones"),
+		      rs.getInt("duracion"));
+		      }
+          } 
+		  catch (SQLException ex){ 
+		   System.out.println("SQLException: " + ex.getMessage());
+		  }
+		  return res;
+	}
+	public static void añadirTurno(int id_Empleado) {
+		//CONSULTA3-> Hay que enviar el turno sabiendo el id_empleado
+		int id_turno;
+		PreparedStatement stmt=null;
+		
+		
 	}
 }
--- src/main/java/ingsoft1920/em/Model/EmpleadoModel.java
+++ src/main/java/ingsoft1920/em/Model/EmpleadoModel.java
@@ -1,5 +1,21 @@
 package ingsoft1920.em.Model;
 
 public class EmpleadoModel {
-
+//esto instancia objetos 
+	private int id_Empleado;
+	private int id_Turno;
+	private String id_Rol;
+	private int estado;
+	
+	//Modelo para la consulta1
+	public EmpleadoModelC1(int id_Empleado, int id_Turno) {
+		this.id_Empleado=id_Empleado;
+		this.id_Turno=id_Turno;
+		}
+	//Modelo para la consulta2
+	public EmpleadoModelC2(int id_Empleado, String id_Rol, int estado) {
+		this.id_Empleado=id_Empleado;
+		this.id_Rol=id_Rol;
+		this.estado=estado;
+	}
 }
