57551a1660b6bca60de6e667f6de34072afd2f27
Rebeca <rebeca.carrasco.moreno@alumnos.upm.es>
2020-02-20 20:44:51 +0100

    Cambios y creacion de DAOs y Models pedidos

---
+++ src/main/java/ingsoft1920/em/DAO/NominaDAO.java
@@ -0,0 +1,30 @@
+package ingsoft1920.em.DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import ingsoft1920.em.Conector.ConectorBBDD;
+
+public class NominaDAO {
+	static Connection conn;
+	public static void asignarNomina(int id_Empleado,int sueldo) {
+		//CONSULTA1-> Metemos en la tabla nomina 
+		if(conn==null) {
+			conn=ConectorBBDD.conectar();
+		}
+		PreparedStatement stmt= null; 
+		  try { 
+			  stmt=conn.prepareStatement("INSERT INTO nomina(id_Empleado,sueldo)"+
+						"values( ? , ?");
+							stmt.setInt(1, id_Empleado);
+							stmt.setInt(2, sueldo);
+							stmt.executeUpdate();
+		   
+        } 
+		  catch (SQLException ex){ 
+		   System.out.println("SQLException: " + ex.getMessage());
+		  }
+		}
+
+}
--- src/main/java/ingsoft1920/em/Model/EmpleadoModelC2.java
+++ src/main/java/ingsoft1920/em/Model/EmpleadoModelC2.java
@@ -2,14 +2,14 @@ package ingsoft1920.em.Model;
 
 public class EmpleadoModelC2 {
 	private int id_Empleado;
-	private String id_Rol;
+	private String nombre;
 	private boolean estado;
 	
 	//Modelo para la consulta2
-	public EmpleadoModelC2(int id_Empleado, String id_Rol, boolean estado) {
+	public EmpleadoModelC2(int id_Empleado, String nombre, boolean estado) {
 		super();
 		this.id_Empleado=id_Empleado;
-		this.id_Rol=id_Rol;
+		this.nombre=nombre;
 		this.estado=estado;
 		}
 
@@ -21,20 +21,20 @@ public class EmpleadoModelC2 {
 		this.id_Empleado = id_Empleado;
 	}
 
-	public String getId_Rol() {
-		return id_Rol;
-	}
-
-	public void setId_Rol(String id_Rol) {
-		this.id_Rol = id_Rol;
-	}
-
 	public boolean getEstado() {
 		return estado;
 	}
 
 	public void setEstado(boolean estado) {
 		this.estado = estado;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
 	}
 
 }
