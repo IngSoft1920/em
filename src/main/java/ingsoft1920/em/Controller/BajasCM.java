d80f7be3672360a004a855e7f2c6e3adb21671e9
Oscar Cabrera <oscar.cabrera.rodriguez@alumnos.upm.es>
2020-02-22 21:02:10 +0100

    edit conexion CM baja/vacacion

--- src/main/java/ingsoft1920/em/Controller/BajasCM.java
+++ src/main/java/ingsoft1920/em/Controller/BajasCM.java
@@ -7,34 +7,25 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
-import ingsoft1920.em.Beans.BajasBean;
-import ingsoft1920.em.Beans.VacacionesBean;
+import ingsoft1920.em.Beans.BajaBean;
+import ingsoft1920.em.Beans.VacacionBean;
 import ingsoft1920.em.Servicios.HttpClient;
 
 public class BajasCM {
-	public static List<BajasBean> peticionPedirBaja(){
+	public static BajaBean peticionPedirBaja(){
 		try {
 			HttpClient client = new HttpClient("http://localhost:7004/getBaja","GET");
 			int respCode = client.getResponseCode();
-			
+			BajaBean res=new BajaBean();
 			if(respCode==200) {
 				String resp = client.getResponseBody();
 				JsonObject obj = (JsonObject) JsonParser.parseString(resp);
-				JsonArray id_BajaLista = obj.get("id_baja").getAsJsonArray();
-				int[] id_Bajas = new int[id_BajaLista.size()];
-				JsonArray id_EmpleadoLista = obj.get("id_empleado").getAsJsonArray();
-				int[] id_Empleados = new int[id_EmpleadoLista.size()];
-				JsonArray duracionBajaLista = obj.get("duracion").getAsJsonArray();
-				String[] duracionBaja = new String[duracionBajaLista.size()];
-				List<BajasBean> lista = new ArrayList<BajasBean>();
+				res.setId_baja(obj.get("id_baja").getAsInt());
+				res.setId_empleado(obj.get("id_empleado").getAsInt());
+				res.setDuracion(obj.get("duracion").getAsString());
+				res.setEstado(obj.get("estado").getAsBoolean());
 				
-				for(int i=0;i<id_BajaLista.size();i++) {
-					id_Bajas[i]=id_BajaLista.get(i).getAsInt();
-					id_Empleados[i]=id_EmpleadoLista.get(i).getAsInt();
-					duracionBaja[i]=duracionBajaLista.get(i).getAsString();
-					lista.add(new BajasBean(id_Bajas[i],id_Empleados[i],duracionBaja[i]));
-				}
-				return lista;	
+				return res;	
 			}
 		}catch(Exception e) {
 			e.printStackTrace();
--- src/main/java/ingsoft1920/em/Controller/VacacionesCM.java
+++ src/main/java/ingsoft1920/em/Controller/VacacionesCM.java
@@ -8,37 +8,24 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 import ingsoft1920.em.Beans.ActividadBean;
-import ingsoft1920.em.Beans.VacacionesBean;
+import ingsoft1920.em.Beans.VacacionBean;
 import ingsoft1920.em.Servicios.HttpClient;
 
 public class VacacionesCM {
-	public static List<VacacionesBean> peticionPedirVacaciones(){
+	public static VacacionBean peticionPedirVacaciones(){
 		try {
-			HttpClient client = new HttpClient("http://localhost:7004/getVacaciones","GET");
+			HttpClient client = new HttpClient("http://localhost:7004/getVacaciones","POST");
 			int respCode = client.getResponseCode();
-			
+			VacacionBean res=new VacacionBean();
 			if(respCode==200) {
 				String resp = client.getResponseBody();
 				JsonObject obj = (JsonObject) JsonParser.parseString(resp);
-				JsonArray id_VacacionesLista = obj.get("id_vacaciones").getAsJsonArray();
-				int[] id_Vacaciones = new int[id_VacacionesLista.size()];
-				JsonArray id_EmpleadoLista = obj.get("id_empleado").getAsJsonArray();
-				int[] id_Empleados = new int[id_EmpleadoLista.size()];
-				JsonArray ListaInicioVacaciones = obj.get("vacacionesInicio").getAsJsonArray();
-				String[] inicioVacaciones = new String[ListaInicioVacaciones.size()];
-				JsonArray ListaFinVacaciones = obj.get("vacacionesFin").getAsJsonArray();
-				String[] finVacaciones = new String[ListaFinVacaciones.size()];
-				List<VacacionesBean> lista = new ArrayList<VacacionesBean>();
+				res.setId_vacacion(obj.get("id_vacaciones").getAsInt());
+				res.setId_empleado(obj.get("id_empleado").getAsInt());
+				res.setDuracion(obj.get("duracion").getAsString());
+				res.setEstado(obj.get("estado").getAsBoolean());
 				
-				for(int i=0;i<id_VacacionesLista.size();i++) {
-					id_Vacaciones[i]=id_VacacionesLista.get(i).getAsInt();
-					id_Empleados[i]=id_EmpleadoLista.get(i).getAsInt();
-					inicioVacaciones[i]=ListaInicioVacaciones.get(i).getAsString();
-					finVacaciones[i]=ListaFinVacaciones.get(i).getAsString();
-					lista.add(new VacacionesBean(id_Vacaciones[i],id_Empleados[i],inicioVacaciones[i],finVacaciones[i]));
-					
-				}
-				return lista;	
+				return res;	
 			}
 		}catch(Exception e) {
 			e.printStackTrace();
