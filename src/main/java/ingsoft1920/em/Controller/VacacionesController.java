b5dcf48a40ab58b60bdc02c88c15f10b8bbd3843
Sergio Palomo <sergio.palomov@alumnos.upm.es>
2020-02-24 18:38:11 +0100

    Vacaciones sin funcionar desde inicio

--- src/main/java/ingsoft1920/em/Controller/VacacionesController.java
+++ src/main/java/ingsoft1920/em/Controller/VacacionesController.java
@@ -1,5 +1,7 @@
 package ingsoft1920.em.Controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.apache.logging.log4j.LogManager;
@@ -9,78 +11,113 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@Controller 
+import ingsoft1920.em.Beans.BajaBean;
+import ingsoft1920.em.Beans.VacacionBean;
+import ingsoft1920.em.DAO.BajaDAO;
+import ingsoft1920.em.DAO.VacacionesDAO;
+import ingsoft1920.em.Model.BajaModel;
+import ingsoft1920.em.Model.VacacionesModel;
+
+@Controller
 public class VacacionesController {
 
-final static Logger logger = LogManager.getLogger(LoginController.class.getName());
-	
-	@GetMapping("/menu5")
+	final static Logger logger = LogManager.getLogger(LoginController.class.getName());
+
+	@GetMapping("/menu7")
 	public String loginp(Model model) {
 		return "menu";
 	}
-	
-	@PostMapping("/menu5")
+
+	@PostMapping("/menu7")
 	public String menup(Model model) {
 		return "menu";
 	}
-	
-	@GetMapping("/nomina5")
+
+	@GetMapping("/nomina7")
 	public String nominap1(Model model) {
 		return "nomina";
 	}
-	
-	@PostMapping("/nomina5")
+
+	@PostMapping("/nomina7")
 	public String nominap(Model model) {
 		return "nomina";
 	}
-	
-	@GetMapping("/perfil5")
+
+	@GetMapping("/perfil7")
 	public String perfilp1(Model model) {
 		return "perfil";
 	}
-	
-	@PostMapping("/perfil5")
+
+	@PostMapping("/perfil7")
 	public String perfilp(Model model) {
 		return "perfil";
 	}
-	
-	@GetMapping("/cerrar5")
+
+	@GetMapping("/cerrar7")
 	public String cerrarp1(Model model) {
 		return "login";
 	}
-	
-	@PostMapping("/cerrar5")
+
+	@PostMapping("/cerrar7")
 	public String cerrarp(Model model) {
 		return "login";
 	}
-	
-	@GetMapping("/tareas5")
+
+	@GetMapping("/tareas7")
 	public String tareasp1(Model model) {
 		return "tareas";
 	}
-	
-	@PostMapping("/tareas5")
+
+	@PostMapping("/tareas7")
 	public String tareasp(Model model) {
 		return "tareas";
 	}
-	
-	@GetMapping("/vacaciones5")
+
+	@GetMapping("/vacaciones7")
 	public String vacacionesp1(Model model) {
 		return "vacaciones";
 	}
-	
-	@PostMapping("/vacaciones5")
+
+	@PostMapping("/vacaciones7")
 	public String vacacionesp(Model model) {
 		return "vacaciones";
 	}
-	
-	@GetMapping("/bajas5")
+
+	@GetMapping("/bajas7")
 	public String bajasp1(Model model) {
 		return "bajas";
 	}
-	
-	@PostMapping("/bajas5")
+
+	@PostMapping("/bajas7")
 	public String bajasp(Model model) {
 		return "bajas";
 	}
-}
+// TODO
+	@GetMapping("/verVacaciones")
+	public String verVacaciones(Model model) {
+		List<VacacionesModel> vacaciones = VacacionesDAO.sacaVacaciones(1);
+		model.addAttribute("bajas", vacaciones);
+		return "muestraVacaciones";//TODO
+	}
+
+	@PostMapping("/verVacaciones")
+	public String verVacaciones1(Model model) {
+		return "muestraVacaciones"; // TODO
+	}
+
+	@GetMapping("/añadeVacaciones")
+	public String añadeBaja(Model model) {
+		VacacionBean vacacion = new VacacionBean();
+		model.addAttribute("vacacionesBean", vacacion);
+		model.addAttribute("mensajeError", "");
+		return "vacaciones";
+	}
+
+	@PostMapping("/añadeVacaciones")
+	public String añadeVacaciones1(BajaBean bajaBean, Model model) {
+		// TO-DO COMPROBAR CAMPOS VALIDOS
+		VacacionBean vacacion = new VacacionBean();
+		VacacionesDAO.editaVacacion(vacacion);
+		return "vacaciones";
+	}
+  }
