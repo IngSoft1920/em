5654531a9b707be4053774203102744fd81b7bbc
Rebeca <rebeca.carrasco.moreno@alumnos.upm.es>
2020-04-27 20:25:19 +0200

    cambio2

--- src/main/java/ingsoft1920/em/DAO/NominaDAO.java
+++ src/main/java/ingsoft1920/em/DAO/NominaDAO.java
@@ -45,7 +45,7 @@ public class NominaDAO {
 		}
 	}
 	
-	public static List<NominaModel> verNomina(int id_Empleado,int id_nomina) {
+	public static List<NominaModel> verNomina(int id_Empleado,int id_mes) {
 		if(conn==null) {
 			conn=ConectorBBDD.conectar();
 		}
@@ -54,9 +54,9 @@ public class NominaDAO {
 		ResultSet rs = null; 
 		
 		try { 
-			stmt=conn.prepareStatement("SELECT * FROM nomina WHERE id_empleado=? and id_nomina=?; ");
+			stmt=conn.prepareStatement("SELECT * FROM nomina WHERE id_empleado=? and id_mes=?; ");
 			stmt.setInt(1, id_Empleado);
-			stmt.setInt(2,id_nomina);
+			stmt.setInt(2,id_mes);
 			rs=stmt.executeQuery();
 			while(rs.next()) {
 				NominaModel nomina=new NominaModel(rs.getInt("id_nomina"),rs.getInt("id_empleado"),rs.getInt("id_incentivo"),rs.getInt("id_sueldo"),rs.getInt("valor"));
